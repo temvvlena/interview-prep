6b98236061b2887ea2d7fbc8a59f74c030bb9d81
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-12-03 22:37:39 -0600

    D4

--- leetcode-easy/Array/maxSubArray.py
+++ leetcode-easy/Array/maxSubArray.py
@@ -13,4 +13,27 @@ class Solution:
 #try coding another solution using the divide and conquer approach, which is more subtle.
 
 # Have not finished this part yet. I will update that tomorrow. 
-        
+# Divide and Conquer Algorithm
+"""
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self.helper(nums, 0, len(nums)-1)
+    
+    def helper(self, nums, low, high):
+        if low > high:
+            return 0
+        if low == high:
+            return nums[low]
+        mid = low + (high-low)//2
+        x_left = self.helper(nums, low, mid)
+        x_right = self.helper(nums, mid+1, high)
+        lmax, rmax = float('-inf'), float('-inf')
+        lsum, rsum = 0,0
+        for i in range(mid-1, low-1, -1): ### Important Insight in NlgN solutions
+            lsum = lsum + nums[i]
+            lmax = max(lmax, lsum)
+        for i in range(mid+1, high+1, 1):
+            rsum = rsum + nums[i]
+            rmax = max(rmax, rsum)
+        return max(x_left, x_right, max(0,lmax)+max(0,rmax)+nums[mid]) 
+"""  
---
+++ leetcode-medium/Array/leetcode-88.py
@@ -0,0 +1,62 @@
+"""
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+"""
+
+
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # (n+m)* log(n+m)
+        #nums1[:] = sorted(nums1[:m] + nums2)
+        
+        # Time complexity : (n+m)
+        # Space complexity : m
+        nums1_copy = nums1[:m]
+        nums1[:] = []
+        p1, p2 = 0, 0
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1.append(nums1_copy[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+        # add the rest
+        if p1 < m:
+            nums1[p1 + p2:] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[p1 + p2:] = nums2[p2:]
+        # Can implement it better without using additional space
+        # two get pointers for nums1 and nums2
+        p1 = m - 1
+        p2 = n - 1
+        # set pointer for nums1
+        p = m + n - 1
+        
+        # while there are still elements to compare
+        """
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] =  nums1[p1]
+                p1 -= 1
+            p -= 1
+        
+        # add missing elements from nums2
+        nums1[:p2 + 1] = nums2[:p2 + 1]
+        """
