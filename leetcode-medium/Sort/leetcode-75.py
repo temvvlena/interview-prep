b14a2773cab47360bf65da6eb595f55ede530489
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-01-03 22:18:22 -0600

    D5

---
+++ leetcode-medium/Sort/leetcode-75.py
@@ -0,0 +1,40 @@
+"""
+Given an array nums with n objects colored red, white, or blue, 
+sort them in-place so that objects of the same color are adjacent, 
+with the colors in the order red, white, and blue.
+Here, we will use the integers 0, 1, and 2 to represent the color 
+red, white, and blue respectively.
+
+Follow up:
+Could you solve this problem without using the library's sort function?
+Could you come up with a one-pass algorithm using only O(1) constant space?
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+Input: nums = [0]
+Output: [0]
+Example 4:
+Input: nums = [1]
+Output: [1]
+"""
+class Solution:
+    def sortColors(self, nums):
+        p0 = curr = 0
+        p2 = len(nums) - 1
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1 
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1  
+            else:
+                curr += 1
+        
+# Time Big(N) and Space Big(1)
+        
