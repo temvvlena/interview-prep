4079c374f0f582d210884acbc891f45c3acf5be0
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-01-06 22:40:55 -0600

    D7

---
+++ leetcode-easy/Recursion/leetcode-1137.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/n-th-tribonacci-number/
+The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.
+Example 1:
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+Example 2:
+Input: n = 25
+Output: 1389537
+"""
+class Solution:
+    def __init__(self):
+        self.caches = {}
+    
+    def tribonacci(self, n):   
+        # 3 -> 0 + 1 + 1 == 2
+        # 4 -> 2 + 1 + 1 == 4
+        # 5 -> 4 + 2 + 1 == 7
+        # 6 -> 7 + 4 + 2 == 13
+        if n < 3: return [0, 1, 1][n]
+        n0, n1, n2 = 0, 1, 1
+        i = 3
+        while i <= n:
+            addition = n0 + n1 + n2
+            n0, n1, n2 = n1, n2, addition
+            i += 1
+        return n2
+        """
+        Big O (N) --> Time and Space
+        # Merge Sort and Quick Sort
+        if n in self.caches:
+            return self.caches[n]
+        if n == 0: return 0
+        if n == 1 or n == 2: return 1
+        a = (self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1))
+        self.caches[n] = a
+        return a
+        """
