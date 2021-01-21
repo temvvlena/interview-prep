ac4cfaf547813da439ba57203985fb4b0e1482ea
Temuulen Erdenebulgan <44326790+erdete01@users.noreply.github.com>
2021-01-21 09:11:55 -0600

    Update leetcode-665.py

--- leetcode-easy/Array/leetcode-665.py
+++ leetcode-easy/Array/leetcode-665.py
@@ -25,3 +25,14 @@ class Solution:
                 fixed = True
             else: prev = n
         return True
+    
+    
+    """
+    Solution 2
+    p = None
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i+1]:
+            if p is not None: return False
+            p = i
+    return (p is None or p == 0 or p == len(nums) - 2 or nums[p] <= nums[p+2] or nums[p+1] >= nums[p-1])
+    """
