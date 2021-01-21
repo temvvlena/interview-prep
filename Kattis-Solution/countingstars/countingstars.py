a47da7d6e1411fbb811961b84fe50a24ee10b3d9
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-01-20 20:56:39 -0600

    Day2

---
+++ Kattis-Solution/countingstars/countingstars.py
@@ -0,0 +1,8 @@
+import sys
+
+def countingstart(row: int, col: int, myString: str) -> int:
+    pass
+
+def main():
+    print(sys.stdin.readline().strip().split())
+main()
---
+++ leetcode-easy/Array/leetcode-665.py
@@ -0,0 +1,27 @@
+"""
+Non-decreasing Array
+Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+Example 1:
+Input: nums = [4,2,3]
+Output: true
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+Input: nums = [4,2,1]
+Output: false
+Explanation: You can't get a non-decreasing array by modify at most one element.
+"""
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        if self.tryFix(nums): return True
+        return self.tryFix(nums[::-1], lambda n,p: p<n)
+    def tryFix(self, nums: List[int], predict=lambda n,p: n<p) -> bool:
+        prev = nums[0]
+        fixed = False
+        for n in nums:
+            if predict(n, prev):
+                if fixed: return False
+                fixed = True
+            else: prev = n
+        return True
