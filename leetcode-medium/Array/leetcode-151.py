522e2b9b17f8fd38b9a875d54dff7687a694f3d0
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-01-28 21:57:11 -0600

    Day24

---
+++ leetcode-medium/Array/leetcode-151.py
@@ -0,0 +1,57 @@
+"""
+Reverse Words in a String
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+Example 4:
+
+Input: s = "  Bob    Loves  Alice   "
+Output: "Alice Loves Bob"
+Example 5:
+
+Input: s = "Alice does not even like bob"
+Output: "bob like even not does Alice"
+
+"""
+
+
+from collections import deque
+class Solution:
+    def reverseWords(self, s:str) -> str:
+        start,end = 0,1
+        myDeque = deque()
+        word = []
+        while start <= len(s):
+            if s[start:end] == " " or start == len(s):
+                myDeque.appendleft("".join(word))
+                word = []
+            else:
+                word.append(s[start])
+            end += 1
+            start = end
+        return " ".join(myDeque)
+
+
+    
+        # Time complexity is O(1)
+        # length is from 1 to 10000
+        # return ' '.join(s.split()[::-1])
