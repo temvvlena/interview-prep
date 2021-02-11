9c1e62af865316144ed47da3631c8eb193a88e08
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-02-10 21:05:35 -0600

    D4

--- leetcode-easy/Trees/leetcode-101.py
+++ leetcode-easy/Trees/leetcode-101.py
@@ -1,52 +1,43 @@
-"""
-https://leetcode.com/problems/symmetric-tree/
-Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
-For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
-    1
-   / \
-  2   2
- / \ / \
-3  4 4  3
-But the following [1,2,2,null,3,null,3] is not:
-
-    1
-   / \
-  2   2
-   \   \
-   3    3
-"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# Time and Space Complexity O(N)
 class Solution:
-    def isSymmetric(self, root):
-        def _symmetric(n1, n2):        
-            if not n1 and not n2: return True
-            if  not n1 or not n2: return False
-            return n1.val == n2.val and _symmetric(n1.left, n2.right) and _symmetric(n1.right, n2.left)
-        return _symmetric(root, root)
-"""
-If the tree is empty, then it is symmetrical to the vertical 
-axis going through its root node.
-
-Else, check if the value at the root node of both subtrees 
-is the same.
-
-If it is, then check if the left subtree and the right 
-subtree are symmetrical.
-
-This can be done by checking if:
-
-4.1 The root nodes of both trees contain the 
-same value.
-
-4.2 The left subtree of the left subtree and the 
-right subtree of the right subtree are symmetrical.
-
-4.3 The right subtree of the left subtree and the 
-left subtree of the right subtree are symmetrical.
-"""
+    def isSymmetric(self, root: TreeNode) -> bool:
+        """
+        1 -> 2 -> 3
+        1 -> 2 -> 4
+        [1,2,2,null,3,null,3]
+        3 case
+        1 -- tentsuu
+        2 -- zuun ni baruun
+        3 -- baruun ni zuun
+        
+        def helper(root1, root2):
+            if root1 is None and root2 is None: return True
+            if root1 is None or root2 is None: return False
+            if root1.val == root2.val: return helper(root1.left, root2.right) and helper(root1.right, root2.left)
+            return False
+        return helper(root, root)
+        Time and Space O(N)
+         """
+        def helper(root1, root2):
+            myStack = []
+            myStack.append(root1)
+            myStack.append(root2)
+            while myStack != []:
+                temp1 = myStack.pop()
+                temp2 = myStack.pop()
+                if temp1 is None and temp2 is None: continue
+                if temp1 is None or temp2 is None: return False
+                if temp1.val != temp2.val: return False
+                myStack.append(temp1.left)
+                myStack.append(temp2.right)
+                myStack.append(temp1.right)
+                myStack.append(temp2.left)
+            return True
+        return helper(root, root)
+       
+        
