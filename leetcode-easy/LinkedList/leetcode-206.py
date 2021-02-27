24dda93e17f8341a7ca9d490e3a8235d54aa9218
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-02-27 01:25:41 -0600

    D27

--- leetcode-easy/LinkedList/leetcode-206.py
+++ leetcode-easy/LinkedList/leetcode-206.py
@@ -1,17 +1,3 @@
-"""
-https://leetcode.com/problems/reverse-linked-list/submissions/
-Reverse Linked List
-Reverse a singly linked list.
-
-Example:
-
-Input: 1->2->3->4->5->NULL
-Output: 5->4->3->2->1->NULL
-Follow up:
-
-A linked list can be reversed either iteratively or recursively. Could you implement both?
-"""
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -20,38 +6,32 @@ A linked list can be reversed either iteratively or recursively. Could you imple
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         """
-        1 2 3 4 5 -> None
-           None
-           
-        1 2 3 4 5 -> None
-        2 1     3 4 5
-        3 2 1    4 5 
-        4 3 2 1   5
-        5 4 3 2 1  None
-        Tail recursion (Read)
+        Recursively
+        1 -> 2 -> 3 -> 4 -> None
+        if not head or not head.next: return head
+        p = self.reverseList(head.next)
+        head.next.next=head
+        head.next = None
+        return p
+        Iterate
+        1 -> 2 -> 3 -> 4 -> None
+        null <- 1 
+        prev = None
+        while not head:
+            temp = head.next
+            head = prev
+            prev = head
+            temp = head
+        return prev
         """
-        newNode = None
-        def helper(newNode, head): 
-            if head is None: 
-                return newNode
+        previousNode = None
+        while head is not None:
             temp = head.next
-            head.next = newNode
-            return helper(head, temp)
-        return helper(newNode, head)
-    
-    """
-        previous = None
-        while head:
-            temp = head.next
-            head.next = previous
-            previous = head
+            head.next = previousNode
+            previousNode = head
             head = temp
-        return previous
-    
+        return previousNode
+            
 
-          Null<-1<-2<-3<-4<-5 -> Null
-               |
-        1 -> 2-> 3-> 4 -> 5 -> Null
-        |
         
-         """
+        
