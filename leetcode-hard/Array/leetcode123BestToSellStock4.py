23383cec7b85da07970fedca5b0c2e8886d41d1c
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-03-09 22:09:56 -0600

    D9

---
+++ leetcode-hard/Array/leetcode123BestToSellStock4.py
@@ -0,0 +1,36 @@
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/solution/
+Time and Space (N) and (Constant)
+Best Time to Buy and Sell Stock III
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete at most two transactions.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+Example 1:
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+Example 2:
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+Example 4:
+Input: prices = [1]
+Output: 0
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        A, B, C, D = -prices[0], float('-inf'), float('-inf'), float('-inf')
+        for price in prices:
+            A = max(A, -price)
+            B = max(B, A+price)
+            C = max(C, B-price)
+            D = max(D, C+price)
+        return D
