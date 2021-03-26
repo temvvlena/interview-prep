951a116e658d83fcb9de13a59af233fad71d864a
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-03-25 19:48:56 -0500

    D25

--- leetcode-easy/LinkedList/leetcode-234.py
+++ leetcode-easy/LinkedList/leetcode-234.py
@@ -22,10 +22,33 @@ Could you do it in O(n) time and O(1) space?
 #         self.next = next
 class Solution:
     def isPalindrome(self, head):
+        if head is None or head.next is None: return True
+        s = head
+        f = head
+        prev = None
+        
+        while f != None and f.next != None: # 1 -> 2 -> 2 -> 3 -> 1
+            f = f.next.next
+            temp = s.next
+            s.next = prev
+            prev = s
+            s = temp
+            
+        if f != None:
+            s = s.next
+            
+        while prev != None and s != None:
+            if prev.val != s.val: return False
+            prev = prev.next
+            s = s.next
+            
+        return True
+
+        """
         myList = []
         while head is not None:
             myList.append(head.val)
             head = head.next
         return myList==myList[::-1]
-        
+        """
     
