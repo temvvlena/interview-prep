8d8791bf697cec29d1ef493fa431cb51dee931ac
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-05-20 13:07:14 -0400

    D20

--- Cracking-the-Coding-Interview/Chapter4/firstCommonAncestor.py
+++ Cracking-the-Coding-Interview/Chapter4/firstCommonAncestor.py
@@ -2,5 +2,51 @@
 Design an algorithm and write code to find the first common ancestor
 of two nodes in a binary tree. Avoid storing additional nodes in a data structure.
 NOTE: This is not necessarily a binary search tree
-
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':      
+        stack = [root]
+        parent = {root:None}
+        while p not in parent or q not in parent:
+            node = stack.pop()
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+        while q not in ancestors:
+            q = parent[q]
+        return q
+        
+        
+    """
+    def __init__(self):
+        self.ans = None
+    
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def helper(currentNode):
+            if not currentNode: return False
+            left = helper(currentNode.left)
+            right = helper(currentNode.right)
+            mid = currentNode == p or currentNode == q
+            if left + right + mid >= 2:
+                self.ans = currentNode
+            return mid or left or right
+        helper(root)
+        return self.ans
+        
+        """
+            
