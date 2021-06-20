b9e5dfdd56dc02fa6aeeb7e92d2c4e2e7cab108a
Temuulen Erdenebulgan <erdete01@luther.edu>
2021-06-19 20:20:09 -0400

    D1

--- Cracking-the-Coding-Interview/Chapter4/4.7buildOrder.py
+++ Cracking-the-Coding-Interview/Chapter4/4.7buildOrder.py
@@ -6,10 +6,36 @@ is no valid build order, return an error.
 Input:
     projects: a, b, c, d, e, f
     dependencies: (a,d), (f, b), (b,d),(f,a), (d,c)
-Output: f, e, a, b, d, c
+Output: f, e, a, b, d, c 
+Output: f, e, b, a, d, c
 """
+from collections import defaultdict
 
-def buildOrder(projects, dependencies):
-    print(projects, dependencies)
+adjacency_list = defaultdict()
+adjacency_list['a'] = 'd'
+adjacency_list['f'] = 'b'
+adjacency_list['b'] = 'd'
+adjacency_list['f'] = 'a'
+adjacency_list['d'] = 'c'
+adjacency_list['c'] = []
+adjacency_list['e'] = []
 
-print(buildOrder(['a', 'b', 'c', 'd', 'e', 'f'], [['a', 'd'], ['f', 'b'], ['b', 'd'], ['f', 'a'], ['d', 'c']]))
+visited_list = defaultdict()
+visited_list['a'] = False
+visited_list['b'] = False
+visited_list['c'] = False
+visited_list['d'] = False
+visited_list['e'] = False
+visited_list['f'] = False
+
+def buildOrder(vertex):
+    if not visited_list[vertex]:
+        visited_list[vertex] = True
+        for neighbor in adjacency_list[vertex]:
+            buildOrder(neighbor)
+        res.insert(0, vertex)
+
+res = []
+for vertex in visited_list:
+    buildOrder(vertex)
+print(res)
