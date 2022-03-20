a228c858e40813fe90cb3ccca4bf6b987b0996ae
Temuulen Erdenebulgan <temuulena@gmail.com>
2022-03-20 18:44:12 -0500

    D8

--- Cracking-the-Coding-Interview/test.py
+++ Cracking-the-Coding-Interview/test.py
@@ -1,6 +1,5 @@
 """
-insert(x): Inserts an item x to the data structure if not already present.
-remove(x): Removes item x from the data structure if present.
+remoge Removes item x from the data structure if present.
 search(x): Searches an item x in the data structure.
 getRandom(): Returns a random element from current set of elements
 """
