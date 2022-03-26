324b4d53f1e5ce1dbb96c48f9e90914ed2be3bf8
Temuulen Erdenebulgan <temuulena@gmail.com>
2022-03-25 21:10:38 -0500

    W2

---
+++ leetcode-easy/Array/leetcode-test.py
@@ -0,0 +1,22 @@
+def iterate(nums):
+    """
+    [[1,2,3],
+     [4,5],
+     [6],
+     [],
+     [7,8,9]] ---> [1,4,6,7,2,5,8,3,9]
+      ^
+    """
+    res = []
+    col = 0
+    n = len(nums)
+    for col in range(1000):
+        temp = False
+        for row in range(n):
+            if len(nums[row]) > col:
+                res.append(nums[row][col])
+                temp = True
+        if temp is False:
+            return res
+print(iterate([[1,2,3],[4,5],[6],[],[7,8,9]]))
+print(iterate([[],[1],[2],[],[7,8,9,10]]))
