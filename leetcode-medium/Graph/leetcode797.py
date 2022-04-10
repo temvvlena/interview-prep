c31358f64d42d37bb9b65795bc219f4faf357306
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-04-10 11:28:47 -0500

    W1

--- leetcode-medium/Graph/leetcode797.py
+++ leetcode-medium/Graph/leetcode797.py
@@ -11,3 +11,26 @@ class Solution:
         output = []
         dfs(0, [0], output)
         return output
+
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        """
+        [4,3,1],[3,2,4],[3],[4],[]
+         0.      1       2.  3. 4
+
+        """
+        def dfs(node):
+            path.append(node)
+            if node == len(graph)-1:
+                paths.append(path.copy())
+                return
+            neighbors = graph[node]
+            for neighbor in neighbors:
+                dfs(neighbor)
+                path.pop()
+
+        if not graph or len(graph) == 1: return 0
+        paths, path = [], []
+        dfs(0)
+        return paths
+
+
