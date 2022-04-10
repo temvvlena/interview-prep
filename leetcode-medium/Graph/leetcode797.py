32b963644661159a94342aa9e84e8f6e3d9052c9
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-04-10 14:20:23 -0500

    W1

--- leetcode-medium/Graph/leetcode797.py
+++ leetcode-medium/Graph/leetcode797.py
@@ -2,6 +2,32 @@
 https://leetcode.com/problems/all-paths-from-source-to-target/
 Leetcode 797
 """
+# BFS
+from collections import deque
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        """
+        [4,3,1],[3,2,4],[3],[4],[]
+         0.      1       2.  3. 4
+
+        """
+        if not graph: return []
+        q = deque()
+        q.append([0])
+        paths = []
+        while q:
+            current_path = q.popleft()
+            node = current_path[-1]
+            for neighbor in graph[node]:
+                temp_path = current_path.copy()
+                temp_path.append(neighbor)
+                if neighbor == len(graph) - 1:
+                    paths.append(temp_path)
+                else:
+                    q.append(temp_path)
+        return paths
+
+#DFS
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         end = len(graph) - 1
@@ -32,5 +58,3 @@ class Solution:
         paths, path = [], []
         dfs(0)
         return paths
-
-
