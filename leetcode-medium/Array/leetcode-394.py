f4c124fe3819f2d6f3a6f956fff7c8e76eb0ead4
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-04-17 21:48:55 -0500

    W1

--- leetcode-medium/Array/leetcode-394.py
+++ leetcode-medium/Array/leetcode-394.py
@@ -1,20 +1,29 @@
 # https://leetcode.com/problems/decode-string/
 # Time Comlexity and Space O(n)
-def decodeString(s):
-    myStack, currentCounter, currentStr = [], 0, ''
-    for c in s: 
-        if c == '[':
-            myStack.append(currentStr)
-            myStack.append(currentCounter)
-            currentStr = ''
-            currentCounter = 0
-        elif c == ']':
-            count = myStack.pop()
-            prevStr = myStack.pop()
-            currentStr = prevStr + count*currentStr 
-        elif c.isnumeric():
+def decodeString(s: str) -> str:
+    stack, currentCounter, currentString = [], 0, ''
+    for c in s:
+        if c == "[":
+            stack.append(currentCounter)
+            stack.append(currentString)
+            currentCounter, currentString = 0, ''
+
+        if c == "]":
+            prev = stack.pop()
+            counter = stack.pop()
+            currentString = prev + counter * currentString
+
+        if c.isnumeric():
             currentCounter = currentCounter * 10 + int(c)
+
         else:
-            currentStr += c
-    return currentStr
+            currentString += c
+    res = []
+    for c in currentString:
+        if c == "[" or c == ']':
+            continue
+        res.append(c)
+    return "".join(res)
+
+
 print(decodeString('31[a2[bc]]'))
