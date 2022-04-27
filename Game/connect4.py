0a3af5a0232456a2e293becedc0bf05dc6779892
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-04-26 22:47:37 -0500

    W2

--- Game/connect4.py
+++ Game/connect4.py
@@ -2,53 +2,111 @@ BOARD_COLS = 7
 BOARD_ROWS = 6
 
 
+# Game board object
 class Board():
     def __init__(self):
         self.board = [[' ' for _ in range(BOARD_COLS)] for _ in range(BOARD_ROWS)]
         self.turns = 0
-        self.last_move = [-1, -1]
+        self.last_move = [-1, -1]  # [r, c]
 
     def print_board(self):
         print("\n")
-        for c in range(BOARD_COLS):
-            print(f" ({c + 1})", end="")
+        # Number the columns seperately to keep it cleaner
+        for r in range(BOARD_COLS):
+            print(f"  ({r + 1}) ", end="")
         print("\n")
-        for row in range(len(self.board)):
-            print('| ', end="")
-            for col in range(len(self.board[0])):
-                print(f"{self.board[row][col]} | ", end="")
-            print('\n')
-        print(f"{'-' * 35}\n")
+
+        # Print the slots of the game board
+        for r in range(BOARD_ROWS):
+            print('|', end="")
+            for c in range(BOARD_COLS):
+                print(f"  {self.board[r][c]}  |", end="")
+            print("\n")
+
+        print(f"{'-' * 42}\n")
 
     def which_turn(self):
-        players = ["X", "O"]
+        players = ['X', 'O']
         return players[self.turns % 2]
 
+    def in_bounds(self, r, c):
+        return (r >= 0 and r < BOARD_ROWS and c >= 0 and c < BOARD_COLS)
+
     def turn(self, column):
-        for r in range(BOARD_ROWS - 1, -1, -1):
-            if self.board[r][column] == " ":
-                self.board[r][column] = self.which_turn()
-                self.last_move = [r, column]
+        # Search bottom up for an open slot
+        for i in range(BOARD_ROWS - 1, -1, -1):
+            if self.board[i][column] == ' ':
+                self.board[i][column] = self.which_turn()
+                self.last_move = [i, column]
+
                 self.turns += 1
                 return True
+
+        return False
+
+    def check_winner(self):
+        last_row = self.last_move[0]
+        last_col = self.last_move[1]
+        last_letter = self.board[last_row][last_col]
+
+        # [r, c] direction, matching letter count, locked bool
+        directions = [[[-1, 0], 0, True],
+                      [[1, 0], 0, True],
+                      [[0, -1], 0, True],
+                      [[0, 1], 0, True],
+                      [[-1, -1], 0, True],
+                      [[1, 1], 0, True],
+                      [[-1, 1], 0, True],
+                      [[1, -1], 0, True]]
+
+        # Search outwards looking for matching pieces
+        for a in range(4):
+            for d in directions:
+                r = last_row + (d[0][0] * (a + 1))
+                c = last_col + (d[0][1] * (a + 1))
+
+                if d[2] and self.in_bounds(r, c) and self.board[r][c] == last_letter:
+                    d[1] += 1
+                else:
+                    # Stop searching in this direction
+                    d[2] = False
+
+        # Check possible direction pairs for '4 pieces in a row'
+        for i in range(0, 7, 2):
+            if (directions[i][1] + directions[i + 1][1] >= 3):
+                self.print_board()
+                print(f"{last_letter} is the winner!")
+                return last_letter
+
+                # Did not find any winners
         return False
 
 
 def play():
-    g = Board()
+    # Initialize the game board
+    game = Board()
 
     game_over = False
     while not game_over:
-        g.print_board()
+        game.print_board()
+
+        # Ask the user for input, but only accept valid turns
         valid_move = False
         while not valid_move:
-            user_move = input(f"{g.which_turn()}'s Turn - pick a column (1-{BOARD_COLS}): ")
+            user_move = input(f"{game.which_turn()}'s Turn - pick a column (1-{BOARD_COLS}): ")
             try:
-                valid_move = g.turn(int(user_move) - 1)
+                valid_move = game.turn(int(user_move) - 1)
             except:
                 print(f"Please choose a number between 1 and {BOARD_COLS}")
-    g.print_board()
+
+        # End the game if there is a winner
+        game_over = game.check_winner()
+
+        # End the game if there is a tie
+        if not any(' ' in x for x in game.board):
+            print("The game is a draw..")
+            return
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     play()
