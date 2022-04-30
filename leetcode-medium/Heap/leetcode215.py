7285754c298bfbe2c0a9a3cfe9534cd2d7eda800
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-04-29 23:59:59 -0500

    W2

--- leetcode-medium/Heap/leetcode215.py
+++ leetcode-medium/Heap/leetcode215.py
@@ -5,6 +5,8 @@ https://leetcode.com/problems/kth-largest-element-in-an-array/
 from heapq import heappop, heapify
 
 
+# Time complexity : O(Nlogk).
+# Space complexity : O(K) to store the heap elements
 class Solution:
     def findKthLargest(self, nums, k: int) -> int:
         out = [-num for num in nums]
@@ -12,3 +14,13 @@ class Solution:
         for i in range(k - 1):
             heappop(out)
         return -out[0]
+
+    """
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i = 0
+        for index in range(len(nums) - 1, -1, -1):
+            if i == k - 1:
+                return nums[index]
+            k -= 1
+    """
---
+++ leetcode-medium/Heap/leetcode347.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/top-k-frequent-elements/
+Top K Frequent Elements
+"""
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        myHash = Counter(nums)
+        res = [[aKey, aValue] for aKey, aValue in myHash.items()]
+        res.sort(key=lambda x: x[1], reverse=True)
+        out = []
+        for i in range(k):
+            out.append(res[i][0])
+        return out
