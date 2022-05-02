e5a69cb2fd823f5aa995dad13636ab057676ab73
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-05-01 22:45:28 -0500

    W2

---
+++ leetcode-easy/Array/leetcode326.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/power-of-three/
+326. Power of Three
+"""
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1: return False
+        while (n % 3 == 0):
+            n /= 3
+        return n == 1
