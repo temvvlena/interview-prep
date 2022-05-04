bd6422fede0c8b10b958fec716f657e1a0d4fdec
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-05-03 23:46:22 -0500

    W3

--- leetcode-easy/LinkedList/leetcode-206.py
+++ leetcode-easy/LinkedList/leetcode-206.py
@@ -7,22 +7,14 @@ class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         """
         Recursively
-        1 -> 2 -> 3 -> 4 -> None
-        if not head or not head.next: return head
-        p = self.reverseList(head.next)
-        head.next.next=head
-        head.next = None
-        return p
-        Iterate
-        1 -> 2 -> 3 -> 4 -> None
-        null <- 1 
-        prev = None
-        while not head:
-            temp = head.next
-            head = prev
-            prev = head
-            temp = head
-        return prev
+        class Solution:
+            def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+                if not head or head.next is None:
+                    return head
+                p = self.reverseList(head.next)
+                head.next.next = head
+                head.next = None
+                return p
         """
         previousNode = None
         while head is not None:
@@ -31,7 +23,3 @@ class Solution:
             previousNode = head
             head = temp
         return previousNode
-            
-
-        
-        
