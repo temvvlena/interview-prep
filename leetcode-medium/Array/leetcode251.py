d9215b3b04ecc68396a8fd5a47522c9cb8e6c6a2
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-05-05 21:59:08 -0500

    W3

--- leetcode-medium/Array/leetcode251.py
+++ leetcode-medium/Array/leetcode251.py
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/flatten-2d-vector/
 251. Flatten 2D Vector
 """
+from typing import List
 
 
 class Vector2D:
@@ -22,6 +23,29 @@ class Vector2D:
 
     def hasNext(self) -> bool:
         return self.position != len(self.vec) - 1
+
+
+class Vector2D:
+
+    def __init__(self, vec: List[List[int]]):
+        self.vector = vec
+        self.outer = 0
+        self.inner = 0
+
+    def advanceToNext(self):
+        while self.outer < len(self.vector) and self.inner == len(self.vector[self.outer]):
+            self.inner = 0
+            self.outer += 1
+
+    def next(self) -> int:
+        self.advanceToNext()
+        result = self.vector[self.outer][self.inner]
+        self.inner += 1
+        return result
+
+    def hasNext(self) -> bool:
+        self.advanceToNext()
+        return len(self.vector) > self.outer
 
 # Your Vector2D object will be instantiated and called as such:
 # obj = Vector2D(vec)
