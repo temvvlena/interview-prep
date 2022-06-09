1a36ea1384e0198a38f0fbc74839c8f0c99a70a8
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-06-08 22:36:47 -0500

    D8

--- leetcode-medium/SlidingWindow/leetcode567PermutationString.py
+++ leetcode-medium/SlidingWindow/leetcode567PermutationString.py
@@ -16,20 +16,22 @@ Time O(Lengof s1 + length of s2)
 Space O(1)
 """
 from collections import Counter
+
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1)>len(s2): return False
+        if len(s1) > len(s2): return False
         s1_counter = Counter(s1)
         s2_counter = Counter()
-        for i in range(len(s2)):
-            s2_counter[s2[i]] += 1
-            if i>=len(s1):
-                # Check 2 conditions
-                # equal to 1 or greater than 1
-                if s2_counter[s2[i-len(s1)]]==1: 
-                    del s2_counter[s2[i-len(s1)]]
-                else: 
-                    s2_counter[s2[i-len(s1)]] -= 1
-            if s1_counter == s2_counter: return True
+        length_s1 = len(s1)
+        for count, value in enumerate(s2):
+            s2_counter[value] += 1
+            if count >= length_s1:
+                leftest_element = s2[count - length_s1]
+                if s2_counter[leftest_element] == 1:
+                    del s2_counter[leftest_element]
+                else:
+                    s2_counter[leftest_element] -= 1
+            if s2_counter == s1_counter:
+                return True
         return False
-            
