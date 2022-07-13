4524d153b207d700276bb0915c5799c2b6221222
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-07-13 18:46:14 -0500

    MinRemove

--- leetcode-medium/Array/leetcode1249MinimumRemoveToMakeValid.py
+++ leetcode-medium/Array/leetcode1249MinimumRemoveToMakeValid.py
@@ -26,35 +26,51 @@ Input: s = "(a(b(c)d)"
 Output: "a(b(c)d)"
 """
 
-class Solution:
-    def minRemoveToMakeValid(self, s: str) -> str:
-        s = list(s)
-        remove = list()
-        stack = []
-        for i, c in enumerate(s):
-            if c not in "()": continue
-            if c=="(": stack.append(i)
-            elif not stack: remove.append(i)
-            else: stack.pop()
-        remove += stack
-        for i in remove[::-1]: s.pop(i)
-        return "".join(s)
-
-
 """
-Solution 2
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+1249. Minimum Remove to Make Valid Parentheses
+"""
+
+
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         def delete_invalid_closing(string, open_symbol, close_symbol):
-            sb, balance = [], 0
+            sb = []
+            balance = 0
             for c in string:
-                if c == open_symbol: balance += 1
+                if c == open_symbol:
+                    balance += 1
                 if c == close_symbol:
-                    if balance == 0: continue
+                    if balance == 0:
+                        continue
                     balance -= 1
                 sb.append(c)
             return "".join(sb)
+
+        # Note that s[::-1] gets the reverse of s.
         s = delete_invalid_closing(s, "(", ")")
         s = delete_invalid_closing(s[::-1], ")", "(")
         return s[::-1]
-"""
+
+    def minRemoveToMakeValid2(self, myString: str) -> str:
+        res = []
+        myString = list(myString)
+        for index, value in enumerate(myString):
+            if value == ")" and not res:
+                res.append([value, index])
+            elif value.isalpha():
+                continue
+            elif res and value == ")" and res[-1][0] == "(":
+                res.pop()
+            else:
+                res.append([value, index])
+        mySet = []
+        for char, index in res:
+            mySet.append(index)
+        final = []
+        for index in range(len(myString)):
+            if index in mySet:
+                continue
+            else:
+                final.append(myString[index])
+        return "".join(final)
