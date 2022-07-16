1a5689025bb8174ddef25f5ec1f0860cf54ddb81
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-07-16 00:17:49 -0500

    Added basic calculator 2

--- leetcode-medium/Array/leetcode-227.py
+++ leetcode-medium/Array/leetcode-227.py
@@ -14,29 +14,35 @@ Input: s = " 3+5 / 2 "
 Output: 5
 """
 
+
 class Solution:
-    def calculate(self, s):
-        """
-        Using Stack, time and space complexity will be O(N)
-        s = "3+2*2"
-                |
-        [3, 4] = 7
-        3-2/2*2
-        [3, -2] = 1
-        myStack = [3, ]
-        """
-        if len(s) == 1: return s[0]
-        num, i, myStack, sign = 0, 0, [], "+"
-        while i < len(s):
-            if s[i].isdigit(): num = num * 10 + int(s[i])
-            if s[i] in "+-*/" or i == len(s)-1:
-                if sign == "+": myStack.append(num)
-                elif sign == "-": myStack.append(-num)
-                elif sign == "*": myStack.append(myStack.pop()*num)
-                else: myStack.append(int(myStack.pop()/num))
-                num, sign = 0, s[i]        
-            i += 1
-        return sum(myStack)
+    def calculate(self, s: str) -> int:
+        stack = []
+        index = 0
+        currentNumber = 0
+        operation = "+"
+        while index < len(s):
+            if s[index].isnumeric():
+                currentNumber = currentNumber * 10 + int(s[index])
+            if s[index] in "*/+-" or index == len(s) - 1:
+                if operation == "-":
+                    stack.append(-currentNumber)
+                elif operation == "+":
+                    stack.append(currentNumber)
+                elif operation == "*":
+                    stack.append(stack.pop() * currentNumber)
+                else:
+                    temp = stack.pop()
+                    if temp < 0:
+                        stack.append(-(-temp // currentNumber))
+                    else:
+                        stack.append(temp // currentNumber)
+                operation = s[index]
+                currentNumber = 0
+            if s[index] == " ":
+                pass
+            index += 1
+        return sum(stack)
 
         # It can be optimized using one loop. With time (N) and Space(1). 
         # Will work on it later
