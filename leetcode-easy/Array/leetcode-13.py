26e206d1479d56469e45f519b0c14b5cc09306d4
Temuulen Erdenebulgan <temuulenab@gmail.com>
2022-08-20 16:17:21 -0500

    Roman to Integer

--- leetcode-easy/Array/leetcode-13.py
+++ leetcode-easy/Array/leetcode-13.py
@@ -1,65 +1,46 @@
-"""
-Time Complexity and Space Complexity: O(1)
-https://leetcode.com/problems/roman-to-integer/
-Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
-C can be placed before D (500) and M (1000) to make 400 and 900.
-Given a roman numeral, convert it to an integer.
-
-Example 1:
-
-Input: s = "III"
-Output: 3
-Example 2:
-
-Input: s = "IV"
-Output: 4
-Example 3:
-
-Input: s = "IX"
-Output: 9
-Example 4:
-
-Input: s = "LVIII"
-Output: 58
-Explanation: L = 50, V= 5, III = 3.
-Example 5:
-
-Input: s = "MCMXCIV"
-Output: 1994
-Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
-
-"""
-
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hashing = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        total = hashing.get(s[-1])
-        for i in reversed(range(len(s) - 1)):
-            if hashing[s[i]] < hashing[s[i+1]]: 
-                total -= hashing[s[i]]
+        # RIGHT TO LEFT
+        symbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        answer = 0
+        index = len(s) - 1
+        while index >= 0:
+            if index - 1 >= 0 and symbols[s[index]] > symbols[s[index - 1]]:
+                answer += (symbols[s[index]] - symbols[s[index - 1]])
+                index -= 2
             else:
-                total += hashing[s[i]]
-        return total 
+                answer += symbols[s[index]]
+                index -= 1
+        return answer
 
-        """
-        hashing = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        ans = 0
-        for i in range(len(s)):
-            if i+1 != len(s) and roman[s[i+1]]>roman[s[i]]: ans -= roman[s[i]]
-            else: ans += roman[s[i]]
-        return ans
-        """
+    # LEFT TO RIGHT
+    def romanToInt2(self, s: str) -> int:
+        symbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        answer = 0
+        i = 0
+        while i < len(s):
+            # Subtractive case
+            if i + 1 < len(s) and symbols[s[i]] < symbols[s[i + 1]]:
+                answer += (symbols[s[i + 1]] - symbols[s[i]])
+                i += 2
+            # Not subtractive case
+            else:
+                answer += symbols[s[i]]
+                i += 1
+        return answer
